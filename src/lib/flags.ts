9de96ef9a346578b53a57216aa0d3d43a3d270d2
Artem Schukin <shuckinart@yandex.ru>
2021-07-24 23:05:14 +0300

    add opacity slider, fix flag margins in chrome 64

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -5,3 +5,11 @@ export const flags = [
     `%3Csvg xmlns='http://www.w3.org/2000/svg' width='800' height='480' %3E%3Cpath fill='%2321b1ff' d='M0 0h800v480H0z'/%3E%3Cpath fill='%23ffd800' d='M0 0h800v320H0z'/%3E%3Cpath fill='%23ff218c' d='M0 0h800v160H0z'/%3E%3C/svg%3E`,
     `%3Csvg xmlns='http://www.w3.org/2000/svg' width='800' height='480' %3E%3Cpath fill='%235bcefa' d='M0 0h800v480H0z'/%3E%3Cpath fill='%23f5a9b8' d='M0 96h800v288H0z'/%3E%3Cpath fill='%23fff' d='M0 192h800v96H0z'/%3E%3C/svg%3E`
 ]
+
+export const flagAlts = [
+    'lesbian',
+    'gay',
+    'bi',
+    'pan',
+    'trans'
+]
